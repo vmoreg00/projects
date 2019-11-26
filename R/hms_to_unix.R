42284aa626561eeb562488729cafcdfe844e834b
vmoreg00 <vmoreg00@estudiantes.unileon.es>
2019-11-26 12:29:31 +0100

    Fix date bug in hms_to_unix

--- R/hms_to_unix.R
+++ R/hms_to_unix.R
@@ -5,9 +5,9 @@
 
 hms_to_unix <- function(hms, date = NULL){
   if(is.null(date)){
-    date <- "01-01-1970"
+    date <- "197-01-01"
   }
-  time <- as.POSIXct(paste(date, hms), tz = "UTC", format = "%d-%m-%Y %H:%M:%S")
+  time <- as.POSIXct(paste(date, hms), tz = "UTC", format = "%Y-%m-%d %H:%M:%S")
   time <- as.numeric(time)
   return(time)
 }
