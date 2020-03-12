f5f39fc08d3279ab0a8b463b0e087abb5e8ff03f
vmoreg00 <vmoreg00@estudiantes.unileon.es>
2020-03-12 10:26:56 +0100

    Minor bugs fixed

--- R/start_project.R
+++ R/start_project.R
@@ -3,19 +3,23 @@
 # This function starts a project directory and all their sub-directories
 # according to an homogeneous logical structure.
 
-start_project <- function(name = NULL, path = "./"){
-  if(is.null(name)){
+start_project <- function(name, path = "./"){
+  if(missing(name)){
     stop("name must be provided")
   }
   if(path != "./"){
     if(!endsWith(path, "/")){
       path <- paste0(path, "/")
     }
-    name <- name(paste0(path, name))
   }
-  for(i in c("data", "results", "reports", "src")){
-    if(dir.exists(paste0(name, i, sep = "/"))){
-      dir.create(paste0(name, i, sep = "/"))
+  project <- paste0(path, name)
+  if(dir.exists(project)){
+    warning("Project already exists.")
+  } else {
+    for(i in c("data", "results", "reports", "src")){
+      if(!dir.exists(paste0(c(project, i), collapse = "/"))){
+        dir.create(paste0(c(project, i), collapse = "/"), recursive = T)
+      }
     }
   }
 }
--- R/translate_color.R
+++ R/translate_color.R
@@ -1,11 +1,11 @@
 translate_color <- function(color){
-  eng <- c("black", "grey", "white", "red", "pink", "green" "blue", "purple", "brown", "orange", "yellow")
+  eng <- c("black", "grey", "white", "red", "pink", "green", "blue", "purple", "brown", "orange", "yellow")
   spa <- c("negr[oa]", "gris", "blanc[oa]", "roj[oa]", "rosa", "verde", "azul", "morad[oa]", "marr[oó]n", "naranja", "amarill[oa]")
 
   change <- FALSE
   for(i in 1:length(spa)){
     g <- grep(pattern = spa[i],
-              x = color
+              x = color,
               ignore.case = TRUE)
     if(length(g) > 0){
       color <- eng[i]
