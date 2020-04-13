b550f6d7b027117dfc4f0538eacda3f55fc8eb83
vmoreg00 <vmoreg00@estudiantes.unileon.es>
2020-04-13 13:59:20 +0200

    start_project function improved

--- R/start_project.R
+++ R/start_project.R
@@ -1,25 +1,32 @@
 # Start a project
 #
-# This function starts a project directory and all their sub-directories
+# Configure a project directory and all their sub-directories
 # according to an homogeneous logical structure.
 
-start_project <- function(name, path = "./"){
-  if(missing(name)){
-    stop("name must be provided")
+start_project <- function(subdirs, git = TRUE){
+  # Create sub-directories
+  if(missing(subdirs)){
+    subdirs <- c("data", "results", "reports", "src")
   }
-  if(path != "./"){
-    if(!endsWith(path, "/")){
-      path <- paste0(path, "/")
+  for(sd in subdirs){
+    if(!dir.exists(sd)){
+      dir.create(sd)
     }
   }
-  project <- paste0(path, name)
-  if(dir.exists(project)){
-    warning("Project already exists.")
-  } else {
-    for(i in c("data", "results", "reports", "src")){
-      if(!dir.exists(paste0(c(project, i), collapse = "/"))){
-        dir.create(paste0(c(project, i), collapse = "/"), recursive = T)
-      }
-    }
+
+  # name of the project
+  projName <- unlist(strsplit(getwd(), split = "/"))
+  projName <- projName[length(projName)]
+
+  # Create README.md file
+  write(x = paste0("# ", projName, "\n\n",
+                   "Created on: ", Sys.Date()),
+        file = "README.md")
+
+  # Init the GIT directory
+  if(git){
+    git2r::init()
+    git2r::add(path = "*")
+    git2r::commit(message = "Project start")
   }
 }
